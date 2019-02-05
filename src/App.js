8b9d4aa011623b07f460fee5a94347062ed90f62
MaximeConan <maximeconan5@gmail.com>
2019-02-05 10:09:29 +0100

    ajout de la traduction textarea -> markdown

--- src/App.js
+++ src/App.js
@@ -4,17 +4,42 @@ import './App.css'
 // J'importe le fichier sampleText qui gèrera le contenu par défaut du textarea
 import { sampleText } from './sampleText'
 
+// J'importe markedJS pour générer l'éditeur de Mardown
+import marked from 'marked'
+
 class App extends Component {
+
+  state = {
+    text: sampleText
+  }
+
+  // Capture l'event sur le textearea
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  // Paramétrage de Markdown
+  renderText = text => {   
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+  
+
   render () {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-sm-6'>
-            <textarea rows='35' className='form-control' value={ sampleText } />
+            <textarea
+             onChange={ this.handleChange }
+             rows='35' className='form-control'
+             value={ this.state.text } />
           </div>
           <div className='col-sm-6'>
-            <h1>Résultat</h1>
-            <div>{ sampleText }</div>
+            <div 
+              dangerouslySetInnerHTML={ this.renderText(this.state.text)}>
+            </div>
           </div>
         </div>
       </div>
